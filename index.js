18e85bdbca6f7be7cd2985a1ea544b8dedbf25f0
tobias <tobias.omondi@student.moringaschool.com>
2023-07-12 11:26:58 +0300

    my assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return 'This one is on me!'
+  }
+  else if (distance >2000 && distance <2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (distance > 2500) {
+    return 'No can do.'
+  }
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+if (city=== ('NYC')) {
+  return 'Ok, sounds good.'
+}
+else {
+  return "No go." 
+}
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if (tip === ('generous')) {
+    return "Thank you so much." 
+  }
+  else if (tip ===('not as generous')) {
+    return "Thank you."
+  }
+  else if (tip ===('if anything else')) {
+  }
+  return "Bye."
 }
